9eba17742ebd19170d490400faca59a2f8d44c27
Pedro Furtado <furtado.pedropaulo@gmail.com>
2023-05-04 17:42:39 -0300

    feat: KafkaAdminConfig

---
+++ payment-service/src/main/java/com/ppfurtado/paymentservice/config/KafkaAdminConfig.java
@@ -0,0 +1,31 @@
+package com.ppfurtado.paymentservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+@Configuration
+public class KafkaAdminConfig {
+
+    private final KafkaProperties properties;
+
+
+    @Bean
+    public KafkaAdmin kafkaAdmin(){
+        var config =  new HashMap<String, Object>();
+        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        return new KafkaAdmin(config);
+    }
+
+    @Bean
+    public KafkaAdmin.NewTopics newTopics(KafkaAdmin kafkaAdmin){
+         return new KafkaAdmin.NewTopics(TopicBuilder.name("payment-topic").partitions(1).build());
+    }
+}
