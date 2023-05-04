9a980688402d185c907cb79b7d7cbcfe79523111
Pedro Furtado <furtado.pedropaulo@gmail.com>
2023-05-04 14:43:30 -0300

    feat: custon listener

--- str-consumer/src/main/java/com/ppfurtado/strconsumer/listeners/StringConsumerListener.java
+++ str-consumer/src/main/java/com/ppfurtado/strconsumer/listeners/StringConsumerListener.java
@@ -1,15 +1,27 @@
 package com.ppfurtado.strconsumer.listeners;
 
+import com.ppfurtado.strconsumer.custom.StrConsumerCustonListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StringConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message){
-        log.info("Receive message {}", message);
+    @StrConsumerCustonListener(groupId = "0")
+    public void create(String message){
+        log.info("CREATE ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustonListener(groupId = "1")
+    public void log(String message){
+        log.info("LOG ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustonListener(groupId = "2", containerFactory = "validMessageContainFactory")
+    public void history(String message){
+        log.info("HISTORY ::: Receive message {}", message);
     }
 }
