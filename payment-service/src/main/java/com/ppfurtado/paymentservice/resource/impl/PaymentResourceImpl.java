cff0c445d29f99d37f4da70a8d00d39608d6c7bd
Pedro Furtado <furtado.pedropaulo@gmail.com>
2023-05-05 14:27:00 -0300

    feat: anotação restcontroller

--- payment-service/src/main/java/com/ppfurtado/paymentservice/resource/impl/PaymentResourceImpl.java
+++ payment-service/src/main/java/com/ppfurtado/paymentservice/resource/impl/PaymentResourceImpl.java
@@ -7,8 +7,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RestController
 @RequestMapping("/payment")
 public class PaymentResourceImpl implements PaymentResource {
 
